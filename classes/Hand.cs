c56d187df5810df75f081f60c38175cb343dcb9c
Jon Fleet <jmfleet1@gmail.com>
2021-05-26 17:57:08 -0400

    Added Hand Class

---
+++ classes/Hand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blackjack.classes
+{
+    class Hand 
+    {
+        public List<Card> Cards { get; set; } = new List<Card>();
+        public int CardTotal 
+        {
+            get
+            {
+                int total = 0;
+                foreach( Card card in Cards)
+                {
+                    total += card.CardValue;
+                }
+                return total;
+            }
+        }
+    }
+}
