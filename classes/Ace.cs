1f84b9c058ba02a005ecd1306f9ed6eb84a64453
Jon Fleet <jmfleet1@gmail.com>
2021-05-27 14:06:46 -0400

    Added Ace Class which inherits card

---
+++ classes/Ace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blackjack.classes
+{
+    class Ace : Card
+    {
+        public Ace() : base("Ace", 11){}
+
+        public void ChangeAceValue(Ace ace)
+        {
+            if(ace.CardValue  == 11)
+            {
+                ace.CardValue = 1;
+            } else
+            {
+                ace.CardValue = 11;
+            }
+        }
+    }
+}
